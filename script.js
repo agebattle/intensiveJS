e31cf6952641cde96b540838e06252e7adac4184
agebattle <118884586+agebattle@users.noreply.github.com>
2025-11-09 15:28:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const contents = document.querySelectorAll('.program-line__content')
+
+contents.forEach((elem) => {
+    const title = elem.querySelector('.program-line__title')
+    const descr = elem.querySelector('.program-line__descr')
+
+title.addEventListener('click', () => {
+    contents.forEach((otherElem) => {
+        const otherDescr = otherElem.querySelector('.program-line__descr')
+        otherDescr.classList.remove('active')
+    })
+    descr.classList.toggle('active')
+})
+
+//    console.dir(descr.classList.remove);
+})
+
+
